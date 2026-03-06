64ccfeafeb244903bd4a03cf30059579ba3564c2
Md. Moniruzzaman Shihab <moniruzzamanshihab.dev@gmail.com>
2026-03-06 23:57:56 +0600

    initial tab style

--- tracker.js
+++ tracker.js
@@ -1,6 +1,10 @@
 const allBtn = document.getElementById("all")
 const openBtn = document.getElementById("open")
 const closedBtn = document.getElementById("closed")
+function initial(){
+    allBtn.classList.add("bg-purple-800","text-white")
+}
+initial()
 
 function reset(){
 allBtn.classList.remove("bg-purple-800","text-white")
