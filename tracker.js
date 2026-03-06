c58424b96537eed748b8c68f5ef1534c50a21e09
Md. Moniruzzaman Shihab <moniruzzamanshihab.dev@gmail.com>
2026-03-06 23:53:27 +0600

    tabs toggling style

---
+++ tracker.js
@@ -0,0 +1,24 @@
+const allBtn = document.getElementById("all")
+const openBtn = document.getElementById("open")
+const closedBtn = document.getElementById("closed")
+
+function reset(){
+allBtn.classList.remove("bg-purple-800","text-white")
+openBtn.classList.remove("bg-purple-800","text-white")
+closedBtn.classList.remove("bg-purple-800","text-white")
+}
+
+allBtn.addEventListener("click",function(){
+reset()
+allBtn.classList.add("bg-purple-800","text-white")
+})
+
+openBtn.addEventListener("click",function(){
+reset()
+openBtn.classList.add("bg-purple-800","text-white")
+})
+
+closedBtn.addEventListener("click",function(){
+reset()
+closedBtn.classList.add("bg-purple-800","text-white")
+})
